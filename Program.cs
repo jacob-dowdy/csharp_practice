9f1e3d1651fc70e7cafadb5b2838b4fed7de373e
Jacob Dowdy <jacobdowdy@rocketmail.com>
2020-07-12 07:41:35 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum = sum + i;
+                }   
+            }
+            Console.WriteLine($"The sume is {sum}");
+        }
+    }
+}
